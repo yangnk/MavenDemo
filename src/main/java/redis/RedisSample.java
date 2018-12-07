aef744c1bed17a5fe2aa3ecded00fb7b12a242d3
yangnk <yangningkai@qq.com>
2018-12-07 09:53:49 +0800

    新增

--- src/main/java/redis/RedisSample.java
+++ src/main/java/redis/RedisSample.java
@@ -10,20 +10,7 @@ import redis.clients.jedis.Jedis;
 public class RedisSample {
     public static void main(String[] args) {
         Jedis jedis = new Jedis("localhost");
-//        System.out.println(jedis.ping());
-        jedis.set("1", "beijing");
-        System.out.println(jedis.get("1"));
+        jedis.set("city", "beijing");
+        System.out.println(jedis.get("city"));
     }
 }
-
-//import redis.clients.jedis.Jedis;
-//
-//public class RedisJava {
-//    public static void main(String[] args) {
-//        //连接本地的 Redis 服务
-//        Jedis jedis = new Jedis("localhost");
-//        System.out.println("连接成功");
-//        //查看服务是否运行
-//        System.out.println("服务正在运行: "+jedis.ping());
-//    }
-//}
