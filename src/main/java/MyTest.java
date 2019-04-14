66d666a46eb7a0c4f1c02df5ae32edbe63c9328b
yangnk <yangnk@citydo.com.cn>
2019-04-14 11:13:44 +0800

    test

---
+++ src/main/java/MyTest.java
@@ -0,0 +1,15 @@
+/**
+ * Created by yangningkai on 2019/4/14.
+ */
+
+/**
+ * @Description:
+ * @Author: yangningkai
+ * @CreateDate: 2019/4/14 10:50
+ * @Version:
+ */
+public class MyTest {
+    public static void main(String[] args) {
+
+    }
+}
